862ba98e7b6440dbcf85eda9985ae1b69a165da4
Prince <itprince4real39@gmail.com>
2021-04-13 00:44:35 +0100

    viewing both on-going and concluded campaign pages completed.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-var myModal = document.getElementById('newCampaign')
+function hideShowDiv() {
+    var x = document.getElementById("sub-main-second");
+    var b = document.getElementById("sub-main");
 
-myModal.addEventListener('shown.bs.modal', function () {
-  
-})
+    if (x.style.display !== "none") {
+        x.style.display = "none";
+        b.style.display = "block";
+    }
+}
+function hideShowDivSecond() {
+    var y = document.getElementById("sub-main");
+    var a = document.getElementById("sub-main-second");
+
+    if (y.style.display !== "none") {
+        y.style.display = "none";
+        a.style.display = "block";
+    }
+}
